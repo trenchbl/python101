df0c5feaee61ec4dc9f98b1b62ec503193b2b135
blago <bl.trenchev@gmail.com>
2019-03-10 22:39:23 +0200

    [Week 1] Count vowels

---
+++ week1.py
@@ -0,0 +1,8 @@
+# # problem 1:
+# s = 'azcbobobegghakl'
+# counter = 0
+# # 'a', 'e', 'i', 'o', and 'u'. F
+# for letter in s:
+#     if letter == 'a' or letter == 'e' or letter == 'i' or letter == 'o' or letter == 'u':
+#         counter += 1
+# print("Number of vowels: ", counter)
